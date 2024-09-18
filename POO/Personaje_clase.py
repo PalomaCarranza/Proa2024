2fa8cc321ffe99492fef0f6ee2d651cff2245386
PalomaCarranza <164367090+PalomaCarranza@users.noreply.github.com>
2024-09-18 11:11:17 -0300

    Add files via upload

--- POO/Personaje_clase.py
+++ POO/Personaje_clase.py
@@ -1,11 +1,37 @@
 class Personaje:
     # Atributo de clase
     estado = True # Vivo
-
+    vida = 100
     # Constructor / Atributo de instancia
-    def__init__(self, nombre, altura, velocidad, recistencia, fuerza):
+    def __init__(self, nombre, altura, velocidad, recistencia, fuerza):
         self.nombre = nombre
         self.altura = altura
         self.velocidad = velocidad
         self.recistencia = recistencia
         self.fuerza = fuerza
+        self.estado = Personaje.estado
+        self.vida = Personaje.vida
+
+    
+    def atacar(self, otro_personaje):
+        if self.estado:
+            danio = self.fuerza - (otro_personaje.resistencia)
+            print(f"{self.nombre} ataca a {otro_personaje.nombre} causando {danio} de daño.")
+            otro_personaje.recibir_danno(danio)
+        else:
+            print(f"{self.nombre} está muerto y no puede atacar.")
+
+    def recibir_danno(self , cantidad):
+        if self.estado:
+            self.vida = self.vida - cantidad
+            print(f"{self.nombre} recibe {cantidad} de daño. Le queda {self.vida} de vida.")
+            if self.vida <= 0: # Si vida es igual o menor a 0 vida es igual a 0 para que no nos de números negativos
+                self.vida = 0
+                print(f"{self.nombre} ha muerto.")
+        else:
+            print(f"{self.nombre} ya está muerto.")
+
+    def mostrar_info(self):
+        print(f"Estado: {self.estado}")
+        print(f"Vida: {self.vida}")
+        print(f"Resistencia: {self.recistencia}")
--- POO/carrera.py
+++ POO/carrera.py
@@ -1,29 +1,49 @@
 from Personaje_clase import Personaje
 
 # nombre, altura, velocidad, recistencia, fuerza
-cantidadPersonaje = 0
+cantidad_personaje = 0
+personajes = []
 while True:
 
   menuInicio = '''
   #########################################
   |     1 - Crear personaje               |
-  |     2 - Carrera                       |
+  |     2 - Jugar                         |
   |     3 - Salir                         |
   ######################################### '''
 
   print(menuInicio)
-  opcion = int(input("Ingrse la opcón seleccionada: "))
+  opcion = int(input("Ingrese la opción seleccionada: "))
   
-  if opcion == 1:
-   cantidadPersonaje = cantidadPersonaje + 1
-     
+  if opcion == 1: # El usuario ingresa todos los atributos del personaje
+    
+    nombre = input("Ingrese el nombre del personaje: ")
+    altura = int(input("Ingrese la altura del personaje (en cm): "))
+    velocidad = int(input("Ingrese la velocidad del personaje: "))
+    resistencia = int(input("Ingrese la resistencia del personaje: "))
+    fuerza = int(input("Ingrese la fuerza del personaje: "))
+
+    nuevo_personaje = Personaje (nombre, altura, velocidad, resistencia, fuerza)
+    personajes.append (nuevo_personaje) # Guarda el personaje en la lista personajes
+
+    cantidad_personaje += 1 # Se suma uno a cantidad_personaje
+    print(f"El personaje {nuevo_personaje.nombre} ha sido creado con éxito.")
+    print(f"Cantidad de personajes creados: {cantidad_personaje}")
 
   elif opcion == 2:
-    print("En proceso")
+    if cantidad_personaje == 0:
+      print("No hay personajes creados para jugar.")
+    else:
+      print("Iniciando la carrera con los siguientes personajes: ")
+      for personaje in personajes:
+        print(f" {personaje.nombre}")
+
     continue
+   
 
-  elif opcion == 3:
-      break
+  elif opcion == 3: # Sale del programa
+    print("Saliendo del programa.")
+    break
 
-p1 = Personaje ("Batman", 180, 80, 90, 70)
-print (f"el personaje se llama {p1}")
+  
+
